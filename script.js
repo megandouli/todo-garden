4e0874249fd33ddf4d5005c0b2e30fe1d7b7d24f
nMDaas <63910563+nMDaas@users.noreply.github.com>
2022-10-02 13:17:40 -0400

    store tasks

--- script.js
+++ script.js
@@ -4,6 +4,18 @@ $(function () {
 
     var tasksList = new Array();
 
+
+    chrome.storage.sync.get(['list1'], function (val) {
+        if (val.list1.length > 0)
+            tasksList = val.list1;
+        console.log("val.list1 :" + val.list1);
+        //displaying the old items
+        for (var i = 0; i < tasksList.length; i++) {
+            addListItem(tasksList[i]);
+        }
+    })
+
+
     $('#addButtonTask').click(function () {
 
         var newTask = $('#taskInput').val();
@@ -15,34 +27,26 @@ $(function () {
         chrome.storage.sync.set({
             'list1': tasksList
         })
-
-        function getList() {
-            console.log("addListItem");
-            document.getElementById("taskInput").value = "";
-            var ul = document.getElementById("todo-listUl");
-    
-            addUI(ul, value, 1)
-        }
-
-        function addListItem(value) {
-            console.log("addListItem");
-            document.getElementById("taskInput").value = "";
-            var ul = document.getElementById("todo-listUl");
-    
-            addUI(ul, value, 1)
-        }
-
-        function addUI(ul, value, num) {
-            var li = document.createElement("li");
-            $("li").addClass("list-group-item");
-            li.appendChild(document.createTextNode(value));
-    
-            if (value === '') {
-                //do nothing
-                //alert("You must write something!");
-            } else {
-                ul.appendChild(li);
-            }}
     
     });
+
+    function addListItem(value) {
+        console.log("addListItem");
+        var ul = document.getElementById("todo-listUl");
+        addUI(ul, value, 1)
+    }
+
+    function addUI(ul, value, num) {
+        var li = document.createElement("li");
+        $("li").addClass("list-group-item");
+        li.appendChild(document.createTextNode(value));
+
+        if (value === '') {
+            //do nothing
+            //alert("You must write something!");
+        } else {
+            ul.appendChild(li);
+        }
+
+    }
 })
